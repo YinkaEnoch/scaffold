90d0709bf4bfcd602c4c4a2818f4911a35a9d69c
yinkaenoch <adedokunyinkaenoch@gmail.com>
2020-06-28 10:48:10 +0100

    [chore] add config executables to main scaffold

--- fed/defaults/src/index.js
+++
@@ -1,6 +0,0 @@
-import { h, render } from "preact";
-import Router from "preact-router";
-import "./index.scss";
-
-const App = () => <h1>Hello World</h1>
-render(<App />, document.querySelector("#root"));
